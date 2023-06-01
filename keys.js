25b132835aa17bf3bb56aeec639de91a3b94af2c
shaqeeb-aziz <mitm-s21-003@superior.edu.pk>
2023-06-01 19:09:15 +0500

    Main

---
+++ keys.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority",
+//     Jwt_secret: "faslkfocvneofu"
+
+// };
+
+module.exports = {
+    mongoUrl: "mongodb://127.0.0.1:27017/Insta",
+    Jwt_secret: "faslkfocvneofu"
+
+};
